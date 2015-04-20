d79f9707b0bf3e01122036e39780c5d61a07191d
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-04-20 16:07:44 +0545

    Added zsh

---
+++ zsh/zsh/aliases.zsh
@@ -0,0 +1,5 @@
+alias sublime='sublime_text'
+alias autochmod='find * -type d -exec chmod 755 {} \;;find * -type f -exec chmod 644 {} \;'
+alias makepot='php ~/.wp/dev/tools/i18n/makepot.php'
+alias pluginpot='makepot wp-plugin'
+alias svnpng='svn propset svn:mime-type image/png *'
