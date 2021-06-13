5503a0007fcc79d3ce1fa492ec932ea6378d487a
Shiva Poudel <3774827+shivapoudel@users.noreply.github.com>
2021-06-13 22:57:22 +0545

    Add phpcs and phpcbf to the env

--- local/install.ps1
+++ local/install.ps1
@@ -1,10 +1,11 @@
 $path          = [Environment]::GetEnvironmentVariable('Path', 'User')
+$vendor        = [Environment]::GetFolderPath('ApplicationData') + "\Roaming\Composer\vendor\bin;"
 $php_path      = [Environment]::GetFolderPath('ApplicationData') + "\Local\lightning-services\php-8.0.0+2\bin\win64;"
 $wp_cli_path   = [Environment]::GetFolderPath('LocalApplicationData') + "\Programs\Local\resources\extraResources\bin\wp-cli\posix;"
 $composer_path = [Environment]::GetFolderPath('LocalApplicationData') + "\Programs\Local\resources\extraResources\bin\composer\posix;"
 
 # Set environment variables.
-[System.Environment]::SetEnvironmentVariable('Path', $path + $php_path + $wp_cli_path + $composer_path, 'User')
+[System.Environment]::SetEnvironmentVariable('Path', $path + $vendor + $php_path + $wp_cli_path + $composer_path, 'User')
 
 # Download PHP Configuration File.
 $php_config = join-path -path $php_path.trim(';') -childpath 'php.ini'
