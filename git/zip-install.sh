e1e8534ca2742c82c8a2e0cedcf372783eed6fdd
Shiva Poudel <3774827+shivapoudel@users.noreply.github.com>
2023-02-20 11:03:15 +0545

    Update zip-install.sh

--- git/zip-install.sh
+++ git/zip-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /tmp/
-curl -SO# http://repo.msys2.org/msys/x86_64/zip-3.0-3-x86_64.pkg.tar.xz
+curl -LSO# http://repo.msys2.org/msys/x86_64/zip-3.0-3-x86_64.pkg.tar.xz
 tar -xvf zip-3.0-3-x86_64.pkg.tar.xz
 mv usr/bin/zip.exe /usr/bin/
